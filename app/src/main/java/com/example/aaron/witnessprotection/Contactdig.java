0a6d0f5286affa24fbb5c3540b27bf446af3d9ce
Aaron Didner <didnera@oregonstate.edu>
2017-12-23 15:53:19 -0800

    Searching through contacts is now autofill

--- app/src/main/java/com/example/aaron/witnessprotection/Contactdig.java
+++ app/src/main/java/com/example/aaron/witnessprotection/Contactdig.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.support.v7.view.menu.MenuBuilder;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -38,10 +39,10 @@ public class Contactdig extends AppCompatActivity {
         final ArrayList<String> items = new ArrayList<String>();
 
         //array adapter for the listview
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.listitem, R.id.itemtextview, items);
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.listitem, R.id.itemtextview, items);
 
         //initializing the listview and adapter
-        ListView listview = (ListView) findViewById(R.id.contactlistview);
+        final ListView listview = (ListView) findViewById(R.id.contactlistview);
         listview.setAdapter(adapter);
 
         //the edittext where the user will enter the name of the person
@@ -57,11 +58,6 @@ public class Contactdig extends AppCompatActivity {
                                         @Override
                                         public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                                        }
-
-                                        @Override
-                                        public void afterTextChanged(Editable s) {
-                                            //after the text is changed
 
                                             //clear the item array
                                             items.clear();
@@ -72,7 +68,7 @@ public class Contactdig extends AppCompatActivity {
                                             CharSequence filter;
                                             filter = name.getText().toString();
 
-                                           Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+                                            Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
                                             while(cursor.moveToNext()){
                                                 String contactname = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
@@ -86,11 +82,26 @@ public class Contactdig extends AppCompatActivity {
                                                     items.add(contactname);
                                                 }
                                             }
+                                            listview.setAdapter(adapter);
+                                            Toast.makeText(getApplicationContext(), "a letter change",
+                                                    Toast.LENGTH_LONG).show();
+
+                                        }
+
+                                        @Override
+                                        public void afterTextChanged(Editable s) {
+                                            //after the text is changed
+
+
                                         }
                                     }
 
 
         );
+
+
+
+
 
         //on item click listener
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -159,10 +170,86 @@ public class Contactdig extends AppCompatActivity {
 
                 }
 
-
-
             }
         });
+
+        /*TextWatcher autoFill = new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //clear the item array
+                items.clear();
+
+                //look through the contacts for any name that contains the same letter and add those to the item array
+                ContentResolver resolver = getContentResolver();
+
+                CharSequence filter=null;
+                filter = name.getText().toString();
+
+                Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+                while(cursor.moveToNext()){
+                    String contactname = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    if(contactname.contains(filter)) {
+                        items.add(contactname);
+                    }
+                    else if((contactname.toLowerCase()).contains(filter)){//case for lower case
+                        items.add(contactname);
+                    }
+                    else if((contactname.toUpperCase()).contains(filter)){//case for uppercase letter
+                        items.add(contactname);
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(), "a letter change",
+                        Toast.LENGTH_LONG).show();
+            }
+
+
+        };
+        name.addTextChangedListener(autoFill);*/
+
+
+        /*name.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                //clear the item array
+                items.clear();
+
+                //look through the contacts for any name that contains the same letter and add those to the item array
+                ContentResolver resolver = getContentResolver();
+
+                CharSequence filter;
+                filter = name.getText().toString();
+
+                Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+                while(cursor.moveToNext()){
+                    String contactname = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    if(contactname.contains(filter)) {
+                        items.add(contactname);
+                    }
+                    else if((contactname.toLowerCase()).contains(filter)){//case for lower case
+                        items.add(contactname);
+                    }
+                    else if((contactname.toUpperCase()).contains(filter)){//case for uppercase letter
+                        items.add(contactname);
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(), "a letter change",
+                        Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });*/
+
     }
 
 
